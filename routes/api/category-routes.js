c9d84485904747104954524d6dbf13b543454bde
Mike <mikej.miller440@gmail.com>
2022-02-21 17:28:49 -0600

    added GET methods for Categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,14 +1,43 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, Tag } = require('../../models');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbCategoryData => {
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock']
+      }
+    ]
+  })
+  .then(dbCategoryData => {
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
   // find one category by its `id` value
   // be sure to include its associated Products
 });
