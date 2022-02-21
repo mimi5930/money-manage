1617a18d4fc6c3ba8f2e6d3412487c9efc97e173
Mike <mikej.miller440@gmail.com>
2022-02-21 16:54:55 -0600

    created connections

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+// require('dotenv').config();
+const pw = require('../pw.json');
 
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(pw.DB_NAME, pw.DB_USER, pw.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
