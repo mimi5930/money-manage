8261b4fa1d489e25d109df57c7666fd74ab7d951
Mike <mikej.miller440@gmail.com>
2022-02-25 20:01:04 -0600

    added update and delete routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,15 +46,62 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  /*
+  expects:
+  {
+    tag_name: 'orange'
+  }
+  */
+  Tag.create(req.body)
+    .then((dbTagData) => {
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'Tag with this id not found' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'Tag ' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
